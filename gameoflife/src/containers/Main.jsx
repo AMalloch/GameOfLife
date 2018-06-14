a15d64b91efa45664fe530a98da9da04646bab9b
Angus Malloch <angusbass@hotmail.com>
2018-06-14 12:20:28 +0100

    Added nested for loop to Grid.jsx to add a 2d array of boxes. Box is undefined atm as the component is not made yet.

--- gameoflife/src/containers/Main.jsx
+++ gameoflife/src/containers/Main.jsx
@@ -4,8 +4,13 @@ import Grid from '../components/Grid.jsx'
 class Main extends React.Component {
   constructor() {
     super();
+    this.speed = 100;
+    this.rows = 30;
+    this.cols = 50;
+
     this.state = {
       generation: 0,
+      gridFull: Array(this.rows).fill().map(() => Array(this.cols).fill(false))
     }
   }
 
@@ -13,7 +18,12 @@ class Main extends React.Component {
     return (
       <div>
         <h1>Game of Life</h1>
-        <Grid />
+        <Grid
+          gridFull={this.state.gridFull}
+          rows={this.rows}
+          cols={this.cols}
+          selectBox={this.selectBox}
+        />
         <h2>Generations: {this.state.generation}</h2>
       </div>
     );
